5a07acf255f4ac3e1e3fb8b98fcda6443efc248c
Yoav Zimmerman <yoavzimmerman@gmail.com>
2017-07-26 21:49:57 -0700

    emojis

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ var contact_clicked = false;
 $("#contact-button").on('click', function(){
     if (contact_clicked) {
         contact_clicked = false;
+        $("#contact-links").fadeToggle(500);
         $("#contact-links").css({"top": "-40px"});
     } else {
         contact_clicked = true;
+        $("#contact-links").fadeToggle(1000);
         $("#contact-links").css({"top": 0});
     }
 });
